60847910547f98902f2528534ed04321cc2c02fb
Rebecca Bracho <rebeccabracho05@gmail.com>
2025-05-29 01:15:19 -0400

    feat: update Card component to truncate long descriptions and enhance styling in header

--- src/components/Card/card.jsx
+++ src/components/Card/card.jsx
@@ -5,15 +5,17 @@ import './card.css';
 
 export default function Card({ title, description, time, onDelete, onEdit, onMoreInfo }) {
     const [isModalOpen, setIsModalOpen] = useState(false);
-
+    const wordLimit = 10;
     const openModal = () => setIsModalOpen(true); 
     const closeModal = () => setIsModalOpen(false);
+
+    const truncatedDescription = description.split(' ').slice(0, wordLimit).join(' ') + (description.split(' ').length > wordLimit ? '...' : '');
 
 
     return (
     <div className='card'>
       <h2>{title}</h2>
-      <p>{description}</p>
+      <p>{truncatedDescription}</p>
       <button onClick={onDelete}>Delete</button>
       <button onClick={onEdit}>Edit</button>
       <button onClick={openModal}>More Info</button>
